42a123c8b78ca54bfd9c0ae2934cbc8d71a440d5
thbins <joons0218@gmail.com>
2026-01-27 17:59:06 +0900

    학습: 변수 선언/할당 및 출력 예제 추가

---
+++ 02-variables/ex4.1/ex4.1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 변수 선언
+	var a int = 10
+	var msg string = "Hello Variable"
+
+	// 변수 값 변경
+	a = 20
+	msg = "Good Morning"
+
+	// 변수 값 출력
+	fmt.Println(msg, a)
+}
