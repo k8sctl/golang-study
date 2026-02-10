c543d68054f30df5d8508ba6f4752a8b42d5cb2e
thbins <joons0218@gmail.com>
2026-02-10 20:33:20 +0900

    학습: 중첩 for문으로 좌측 정렬 별 삼각형(1~5개) 출력 예제 추가

---
+++ 09-for/ex9.5/ex9.5.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i++ {
+		for j := 0; j < i+1; j++ {
+			fmt.Print('*')
+		}
+		fmt.Println()
+	}
+}
+
+/*
+	[출력 결과]
+	*
+	**
+	***
+	****
+	*****
+*/
